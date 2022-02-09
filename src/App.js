f2743cc3148dffa6f6603a64db03c2ab69a7e192
chips556 <lohqihern@hotmail.com>
2022-02-09 21:54:01 +0800

    Added skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [playerOneTurn, setPlayerOneTurn] = useState(true);
+  const [playerOneScore, setPlayerOneScore] = useState(0);
+  const [playerTwoScore, setPlayerTwoScore] = useState(0);
+  const [playerOneDice, setPlayerOneDice] = useState("-");
+  const [playerTwoDice, setPlayerTwoDice] = useState("-");
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Player {playerOneTurn ? "1" : "2"} Turn</h1>
+
+        <div id="scores">
+          <div className="score-and-dice">
+            Score: {playerOneScore}
+            <div className="score">{playerOneDice}</div>
+          </div>
+          <div className="score-and-dice">
+            Score: {playerTwoScore}
+            <div className="score">{playerTwoDice}</div>
+          </div>
+        </div>
+
+        <button>Roll Dice</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
